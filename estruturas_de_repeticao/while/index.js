55cf945600425b1c0a76520a68a3933fab29ee93
Daniel Silveira <danielsilveira.contato@gmail.com>
2021-05-20 08:41:13 -0300

    Estruturas de repetição: while

---
+++ estruturas_de_repeticao/while/index.js
@@ -0,0 +1,15 @@
+// Estrutura de repetição
+// while
+// let i = 0
+
+// while(i < 10) {
+//   console.log(i)
+//   i++
+// }
+// Quando não sabemos o momento da parada, usamos o while
+let i = 7898971914
+
+while(i > 10) {
+  console.log(i)
+  i/=35
+}
